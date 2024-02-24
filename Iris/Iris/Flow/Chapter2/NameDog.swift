d17a1ec29f31d53672ffa526e552ed511f08952b
marina-yamaguti <marina.geller.yamaguti@gmail.com>
2024-02-24 18:00:05 -0300

    feature: name dog

--- Iris/Iris/Flow/Chapter2/NameDog.swift
+++ Iris/Iris/Flow/Chapter2/NameDog.swift
@@ -8,8 +8,9 @@
 import SwiftUI
 
 struct NameDog: View {
-    @State var dogName: String = "Max"
-
+    @State var dogName: String = ""
+    @State private var isEditing: Bool = false
+    
     var body: some View {
         NavigationStack {
             GeometryReader { geometry in
@@ -17,7 +18,7 @@ struct NameDog: View {
                     Color.secondBG
                         .ignoresSafeArea()
                     VStack (alignment: .center, spacing: 40){
-                         Text("meet \(dogName)!")
+                        Text("what will be his name?")
                             .font(Font.titleFont())
                             .foregroundColor(Color.black1)
                         Image("chapter2-nameDog")
@@ -26,18 +27,63 @@ struct NameDog: View {
                             .overlay(Rectangle()
                                 .stroke(Color.black1, lineWidth: 6))
                         
+                        TextField("", text: $dogName, onEditingChanged: { editing in
+                            isEditing = editing
+                        })
+                            .multilineTextAlignment(.center)
+                            .placeholder(when: dogName.isEmpty) {
+                                HStack (spacing: 20) {
+                                    Text("write name").foregroundColor(Color.gray1)
+                                    Image(systemName: "pencil")
+                                        .resizable()
+                                        .frame(width: 21, height: 28)
+                                        .foregroundColor(Color.gray1)
+                                }
+                            }
+                            .padding(40)
+                            .font(Font.titleFont())
+                            .foregroundColor(Color.black1)
+                            .background(
+                                ZStack {
+                                    Rectangle()
+                                        .foregroundColor(.clear)
+                                        .frame(width: 800, height: 99)
+                                        .cornerRadius(90)
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 90)
+                                                .inset(by: 3)
+                                                .stroke(Color.black1, style: StrokeStyle(lineWidth: 6, dash: [16, 16]))
+                                        )
+                                }
+                            )
                     }
                     .foregroundColor(Color.black1)
                     .padding()
+                    if !dogName.isEmpty && isEditing == false {
                         NextButton(destination: Popup3(dogName: dogName))
-                     .position(x: geometry.size.width / 2, y:  geometry.size.height - 100)
-                     
+                            .position(x: geometry.size.width / 2, y:  geometry.size.height - 100)
+                    }
                 }
                 .navigationBarBackButtonHidden(true)
             }
         }
     }
 }
+
+
 #Preview {
     NameDog()
+}
+
+extension View {
+    func placeholder<Content: View>(
+        when shouldShow: Bool,
+        alignment: Alignment = .center,
+        @ViewBuilder placeholder: () -> Content) -> some View {
+            
+            ZStack(alignment: alignment) {
+                placeholder().opacity(shouldShow ? 1 : 0)
+                self
+            }
+        }
 }
