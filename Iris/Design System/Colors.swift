45e7209ec25cd09761bd94409c4a733372725cbf
marina-yamaguti <marina.geller.yamaguti@gmail.com>
2024-02-24 16:00:48 -0300

    feature: design system

---
+++ Iris/Design System/Colors.swift
@@ -0,0 +1,32 @@
+//
+//  Colors.swift
+//  Iris
+//
+//  Created by Marina Yamaguti on 24/02/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    //brand colors
+    public static let mainPink: Color = Color(red: 0.97, green: 0.7, blue: 0.8)
+    public static let brandPurple: Color = Color(red: 0.81, green: 0.71, blue: 0.98)
+    public static let brandPeach: Color = Color(red: 1, green: 0.8, blue: 0.65)
+    public static let brandBlue: Color = Color(red: 0.76, green: 0.87, blue: 0.89)
+    public static let brandLime: Color = Color(red: 0.73, green: 0.72, blue: 0.35)
+    public static let brandGreen: Color = Color(red: 0.33, green: 0.53, blue: 0.29)
+    public static let brandYellow: Color = Color(red: 1, green: 0.83, blue: 0.43)
+    public static let brandMustard: Color = Color(red: 0.95, green: 0.65, blue: 0.2)
+    public static let brandRed: Color = Color(red: 0.93, green: 0.35, blue: 0.25)
+    
+    //background colors
+    public static let mainBG: Color = Color(red: 0.99, green: 0.96, blue: 0.92)
+    public static let secondBG: Color = Color(red: 0.95, green: 0.91, blue: 0.85)
+    
+    //blacks and grays
+    public static let black1: Color = Color(red: 0.11, green: 0.11, blue: 0.11)
+    public static let black2: Color = Color(red: 0.16, green: 0.16, blue: 0.16)
+    public static let gray1: Color = Color(red: 0.51, green: 0.51, blue: 0.51)
+    public static let lightGray: Color = Color(red: 0.85, green: 0.85, blue: 0.85)
+}
---
+++ Iris/Design System/Fonts.swift
@@ -0,0 +1,44 @@
+//
+//  Fonts.swift
+//  Iris
+//
+//  Created by Marina Yamaguti on 24/02/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Font {
+    static func largeTitleFont() -> Font {
+        return Font.custom("OpenDyslexic3", size: 120)
+            .weight(.bold)
+    }
+    
+    static func endingFont() -> Font {
+        return Font.custom("OpenDyslexic3", size: 100)
+            .weight(.bold)
+    }
+    
+    static func mediumTitleFont() -> Font {
+        return Font.custom("OpenDyslexic3", size: 64)
+            .weight(.bold)
+    }
+    
+    static func titleFont() -> Font {
+        return Font.custom("OpenDyslexic3", size: 35)
+                .weight(.bold)
+    }
+    
+    static func regularFont() -> Font {
+        return Font.custom("OpenDyslexic3", size: 35)
+    }
+    
+    static func bodyFont() -> Font {
+        return Font.custom("OpenDyslexic3", size: 24)
+           .weight(.bold)
+    }
+    
+    static func textFont() -> Font {
+        return Font.custom("OpenDyslexic3", size: 24)
+    }
+}
