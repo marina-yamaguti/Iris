5bbccc68a9165339823a101b88e967616e529407
marina-yamaguti <marina.geller.yamaguti@gmail.com>
2024-02-24 15:55:23 -0300

    feature: project and assets

---
+++ Iris/Iris/IrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  IrisApp.swift
+//  Iris
+//
+//  Created by Marina Yamaguti on 24/02/24.
+//
+
+import SwiftUI
+
+@main
+struct IrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
